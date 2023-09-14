e5e390e89d8065c203bc0d05e9777e42a7ae26c1
AdrienDubostIsitech <adrien.dubost@ecole-isitech.fr>
2023-09-14 09:52:12 +0200

    [FEAT] Salle3Feature fonctionnel

--- Assets/app/scripts/Room3LaserManagement.cs
+++ Assets/app/scripts/Room3LaserManagement.cs
@@ -17,6 +17,32 @@ public class Room3LaserManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        launchLaserRay();
+    }
+
+    public void launchLaserRay()
+    {
+        foreach (GameObject laser in this.lasers)
+        {
+            int iteration = lasers.IndexOf(laser); 
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(laser.transform.position, laser.gameObject.transform.forward, out hit, 3))
+            {
+                if (hit.collider.gameObject.tag == "receptor")
+                {
+                    Debug.DrawRay(laser.transform.position, laser.gameObject.transform.forward * 3f, Color.red);
+                    Debug.Log("Did Hit");
+                    Debug.Log(iteration + "marche");
+                }
+                else
+                {
+                    Debug.DrawRay(laser.transform.position, laser.gameObject.transform.forward * 3f, Color.blue);
+                    Debug.Log("Did not Hit");
+                    Debug.Log(iteration + "marche pas");
+                }
+                
+            }
+        }
     }
 }
